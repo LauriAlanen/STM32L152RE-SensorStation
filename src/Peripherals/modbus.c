7781b2511b438f974ec59e262cd946178aff46a0
lauri <laurialanen1@gmail.com>
2025-02-17 13:01:08 +0200

    Added modbus files

---
+++ src/Peripherals/modbus.c
@@ -0,0 +1,8 @@
+/*
+ * modbus.c
+ *
+ *  Created on: 17 Feb 2025
+ *      Author: lauri
+ */
+
+
---
+++ src/Peripherals/modbus.h
@@ -0,0 +1,13 @@
+/*
+ * modbus.h
+ *
+ *  Created on: 17 Feb 2025
+ *      Author: lauri
+ */
+
+#ifndef PERIPHERALS_MODBUS_H_
+#define PERIPHERALS_MODBUS_H_
+
+
+
+#endif /* PERIPHERALS_MODBUS_H_ */
