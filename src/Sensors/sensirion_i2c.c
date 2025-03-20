a2cf86342bd53d2377f0d9e953f1b2972b38057b
lauri <laurialanen1@gmail.com>
2025-03-20 15:01:31 +0200

    Moved all initialization stuff under the init function

--- src/Sensors/sensirion_i2c.c
+++ src/Sensors/sensirion_i2c.c
@@ -7,15 +7,57 @@
 
 #include "sensirion_i2c.h"
 #include "timing.h"
+#include "sgp30.h"
+#include "usart.h"
+
+#define SGP30_CONN_RETRIES 5
 
 int16_t sensirion_i2c_select_bus(uint8_t bus_idx)
 {
     return 1;
 }
 
-void sensirion_i2c_init(void)
+uint8_t sensirion_i2c_init(void)
 {
+    int16_t probe, err = 0;
+    uint8_t init_status = 1;
+
     I2C1_Init();
+
+    for (int i = 0; i < SGP30_CONN_RETRIES; ++i)
+    {
+        probe = sgp30_probe();
+
+        if (probe == STATUS_OK)
+        {
+        	init_status = 0;
+        	break;
+        }
+
+        if (probe == SGP30_ERR_UNSUPPORTED_FEATURE_SET)
+            USART2_write_buffer("Your sensor needs at least feature set version 1.0 (0x20)\r\n");
+
+        USART2_write_buffer("SGP sensor probing failed\r\n");
+        sensirion_sleep_usec(1000000);
+    }
+
+    if(init_status)
+    {
+    	return 1; // Error occured during init
+    }
+
+    err = sgp30_iaq_init();
+    if (err == STATUS_OK)
+    {
+        USART2_write_buffer("sgp30_iaq_init done\r\n");
+    }
+
+    else
+    {
+        USART2_write_buffer("sgp30_iaq_init failed!\r\n");
+    }
+
+    return 0;
 }
 
 void sensirion_i2c_release(void)
